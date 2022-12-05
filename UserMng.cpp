a54a3d884f1a1bbac14eb3fc11d6031dab9d1444
DeathGun5201 <109401063+DeathGun5201@users.noreply.github.com>
2022-12-05 18:52:59 +0800

    第二次修改

--- UserMng.cpp
+++ UserMng.cpp
@@ -1,14 +1,52 @@
 #include"LinkList.h"
 #include<iostream>
 using namespace std;
+#include<string.h>
+#include"UserMng.h"
 
 void AddUser(LinkList &L)
 {
-	printf("ok");
+	int n,i;
+	cout << "请输入用户的个数" << endl;
+	cin >> n;
+	L = new LNode;
+	L->next = NULL;//L为头结点
+	for (i = 0; i < n; i++)
+	{
+		LNode* p = new LNode;
+		while (1)
+		{
+			cout << "请输入第" << i << "个用户的手机号" << endl;
+			cin >> p->data.id;//p->data这里用的是指针类型，但是后面由于结构体嵌套不再是指针，用"."即可！
+			//cout << p->data.id << endl;
+			//cout << sizeof(p->data.id) << endl;
+			IsValid_UserId(p->data.id);
+
+			if (IsValid_UserId(p->data.id) == false)
+				printf("输入格式错误！请重新输入！\n");
+			else
+				break;
+
+		}
+		cout << "请输入第" << i << "个用户的昵称" << endl;
+		cin >> p->data.name;
+		cout << "请输入第" << i << "个用户的性别" << endl;
+		cin >> p->data.sex;
+		cout << "请输入第" << i << "个用户的年龄" << endl;
+		cin >> p->data.age;
+		p->next = L->next;
+		p = L->next;
+	}
 }
 
 bool IsValid_UserId(string Id)
 {
+	int num = size(Id);
+	//cout << num << endl;
+	if (num == 12)
+		return true;
+	else
+		return false;
 	return 0;
 }
 
--- UserMng.h
+++ UserMng.h
@@ -5,4 +5,4 @@
 #include"LinkList.h"
 
 void AddUser(LinkList& L);//添加用户
-int IsValid_UserId(string Id);//判断用户号（手机号）是否合法
+bool IsValid_UserId(string Id);//判断用户号（手机号）是否合法
