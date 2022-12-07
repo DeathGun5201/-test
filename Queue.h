eaa97946861503520af3e2861624ca5cf3431279
DeathGun5201 <109401063+DeathGun5201@users.noreply.github.com>
2022-12-07 09:40:25 +0800

    第六次修改

--- Queue.h
+++ Queue.h
@@ -2,5 +2,6 @@
 #include"LinkList.h"
 //存放有关队列的函数头声明
 Status CreatQ(SqQueue& Q);//构造空队列
-Status EnQueue(SqQueue& Q);
-Status ReviseQueue(SqQueue& Q);//修改当天的运动数据
+Status QueueLength(SqQueue Q);//计算队列长度
+Status EnQueue(SqQueue& Q, int e);//向队尾插入新元素
+status DeQueue(SqQueue& Q);//删除队头元素
--- queue.cpp
+++ queue.cpp
@@ -4,7 +4,7 @@
 
 Status CreatQ(SqQueue& Q)
 {
-	Q.base = new int[8];//留一个空间方便判断是否是满的
+	Q.base = new int[8] ;//留一个空间方便判断是否是满的
 	if (!Q.base)
 	{
 		cout << "程序初始化错误即将退出";
@@ -12,5 +12,28 @@ Status CreatQ(SqQueue& Q)
 		exit(OVERFLOW);
 	}
 	Q.head = Q.rear = 0;
+	Q.base = { 0 };
+	return OK;
+}
+
+Status QueueLength(SqQueue Q)//计算队列长度
+{
+	return (Q.rear - Q.head + 8) % 8;
+}
+
+Status EnQueue(SqQueue& Q, int e)//向队尾插入新元素
+{
+	if ((Q.rear + 1) % 8 == Q.head)
+		return ERROR;
+	Q.base[Q.rear] = e;
+	Q.rear = (Q.rear + 1) % 8;
+	return OK;
+}
+
+Status DeQueue(SqQueue& Q)//删除队头元素
+{
+	if (Q.head == Q.rear)
+		return ERROR;
+	Q.head = (Q.head + 1) % 8;
 	return OK;
 }
